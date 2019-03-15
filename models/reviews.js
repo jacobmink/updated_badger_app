6b7892126ae9a67f5f1ac0af8a7ecd03a539161a
Jacob Mink <jacobmink@gmail.com>
2019-03-15 09:40:27 -0600

    reviewee_id fix

--- models/reviews.js
+++ models/reviews.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
  const reviewSchema = mongoose.Schema({
-     reviewee_id:       {type: String, required: true, unique: true},
+     reviewee_id:       {type: String, required: true},
      text:              {type: String, required: true},
      stars:             {type: String, required: true}
  });
